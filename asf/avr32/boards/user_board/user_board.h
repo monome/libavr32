ebb7a9b21bcaab2975177d8f2cacada74d3e7cad
Sam Doshi <sam@metal-fish.co.uk>
2016-03-23 14:18:17 +0000

    add (diet) asf 3.30

---
+++ asf/avr32/boards/user_board/user_board.h
@@ -0,0 +1,20 @@
+/**
+ * \file
+ *
+ * \brief User board definition template
+ *
+ */
+/*
+ * Support and FAQ: visit <a href="http://www.atmel.com/design-support/">Atmel Support</a>
+ */
+
+#ifndef USER_BOARD_H
+#define USER_BOARD_H
+
+/* This file is intended to contain definitions and configuration details for
+ * features and devices that are available on the board, e.g., frequency and
+ * startup time for an external crystal, external memory devices, LED and USART
+ * pins.
+ */
+
+#endif // USER_BOARD_H
