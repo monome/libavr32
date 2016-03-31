e642d19b39713ce3f6d6bcff49d03216d2480cf9
brian crabtree <tehn@monome.org>
2016-03-31 14:33:43 -0400

    i2c slave tx queue and master rx

--- src/i2c.h
+++ src/i2c.h
@@ -2,39 +2,21 @@
 #define _I2C_H_
 
 
+#define STX 12
+#define ETX 13
+#define DLE 27
 
 extern void i2c_master_tx(uint8_t, uint8_t, int);
-
-
+extern void i2c_master_rx(uint8_t addr, uint8_t idx);
 
 extern void twi_slave_rx( U8 u8_value );
 extern U8 twi_slave_tx( void );
 extern void twi_slave_stop( void );
 
+extern void ii_tx_queue(uint8_t);
+
 typedef void (*process_ii_t)(uint8_t, int);
 extern volatile process_ii_t process_ii;
-
-
-
-// #define I2C_BUFFER_SIZE		1024
-
-// extern volatile u8 i2c_buffer[I2C_BUFFER_SIZE];
-
-
-// // transmit data in slave mode
-// extern u8 i2c_slave_tx(void);
-// // handler for slave rx events
-// extern void i2c_slave_rx(u8 value);
-
-// // slave stop function
-// extern void i2c_slave_stop(void);
-
-// // master send
-// extern void i2c_master_tx(u8* tx);
-// // master receive
-// extern void i2c_master_rx(u8* rx);
-
-
 
 
 #endif // header guard
