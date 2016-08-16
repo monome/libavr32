417074737160a10c2f4ab4d4822b1317094e4555
brian crabtree <tehn@monome.org>
2016-08-16 17:49:35 -0400

    dac slew functions

--- src/dac.h
+++ src/dac.h
@@ -3,9 +3,21 @@
 
 #include "compiler.h"
 
-extern void init_dacs(void);
+#define DAC_RATE_CV 6
+#define DAC_10V 16383
 
-extern void update_dacs(uint16_t *d);
+void init_dacs(void);
+void reset_dacs(void);
 
+void update_dacs(uint16_t *d);
+
+void dac_set_value_noslew(uint8_t n, uint16_t v);
+void dac_set_value(uint8_t n, uint16_t v);
+void dac_set_slew(uint8_t n, uint16_t s);
+void dac_set_off(uint8_t n, int16_t o);
+
+void dac_timer_update(void);
+
+bool dac_is_slewing(uint8_t n);
 
 #endif
