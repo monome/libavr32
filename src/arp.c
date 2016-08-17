53e62c8d4a259d53ab9e9d967c781de1352b40b9
Greg Wuller <greg@afofo.com>
2016-08-16 23:08:54 -0700

    progress, flesh out arp player

--- src/arp.c
+++ src/arp.c
@@ -3,20 +3,21 @@
 
 #include "arp.h"
 
-//
-// prototypes
-//
-static void arp_seq_build_up(arp_seq_t *s);
-static void arp_seq_build_down(arp_seq_t *s);
-static void arp_seq_build_converge(arp_seq_t *s);
-static void arp_seq_build_diverge(arp_seq_t *s);
-static void arp_seq_build_random(arp_seq_t *s);
-static void arp_seq_build_played(arp_seq_t *s);
+//-----------------------------
+//----- functions
+
+static void arp_seq_build_up(arp_seq_t *s, chord_t *c);
+static void arp_seq_build_down(arp_seq_t *s, chord_t *c);
+static void arp_seq_build_converge(arp_seq_t *s, chord_t *c);
+static void arp_seq_build_diverge(arp_seq_t *s, chord_t *c);
+static void arp_seq_build_random(arp_seq_t *s, chord_t *c);
+static void arp_seq_build_played(arp_seq_t *s, chord_t *c);
 
 
-//
-// chord implementaion; pick different name? as values aren't harmonically related... yet
-//
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+///// chord implementation
+
 void chord_init(chord_t *c) {
 	for (u8 i = 0; i < CHORD_MAX_NOTES; i++) {
 		c->notes[i].num = CHORD_NOTE_MAX;
@@ -94,97 +95,86 @@ s8 chord_note_high(chord_t *c) {
 	return c->note_count ? c->notes[c->note_count - 1].num : -1;
 }
 
-
-//
-// arp implementation
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+///// arp seq: implementation
 
 void arp_seq_init(arp_seq_t* s) {
-	chord_init(&(s->chord));
-
-	s->style = eStyleUp;
-
 	for (u8 i = 0; i < ARP_MAX_LENGTH; i++) {
+		s->seq[i].note.num = 0;
+		s->seq[i].note.vel = 0;
 		s->seq[i].empty = 1;
-		s->seq[i].note_idx = 0;
 		s->seq[i].gate_length = 32; // ~1/4 of a beat?
 	}
+	s->length = 0;
 }
 
-bool arp_seq_note_add(arp_seq_t *s, u8 num, u8 vel) {
-	// adds a note to the chord, rebuilds seq?, resets seq
-	return false;
-}
-
-bool arp_seq_note_release(arp_seq_t *s, u8 num) {
-	// removes a note from the chord, rebuilds seq, resets position
-	return false;
-}
-
-void arp_seq_build(arp_seq_t *s, arp_style style) {
-
+void arp_seq_build(arp_seq_t *s, arp_style style, chord_t *c) {
 	for (u8 i = 0; i < ARP_MAX_LENGTH; i++) {
 		s->seq[i].empty = 1;
 	}
 
 	switch (style) {
 	case eStyleUp:
-		arp_seq_build_up(s);
+		arp_seq_build_up(s, c);
 		break;
 	case eStyleDown:
-		arp_seq_build_down(s);
+		arp_seq_build_down(s, c);
 		break;
 	case eStyleUpDown:
-		arp_seq_build_up(s);
+		arp_seq_build_up(s, c);
 		break;
 	case eStyleUpAndDown:
 		// TODO
 		break;
 	case eStyleConverge:
-		arp_seq_build_converge(s);
+		arp_seq_build_converge(s, c);
 		break;
 	case eStyleDiverge:
-		arp_seq_build_diverge(s);
+		arp_seq_build_diverge(s, c);
 		break;
 	case eStyleRandom:
-		arp_seq_build_random(s);
+		arp_seq_build_random(s, c);
 		break;
 	case eStylePlayed:
 		// TODO: we loose the note order currently
-		arp_seq_build_played(s);
+		arp_seq_build_played(s, c);
 		break;
 	}
 }
 
-static void arp_seq_build_up(arp_seq_t *s) {
+static void arp_seq_build_up(arp_seq_t *s, chord_t *c) {
 	s->style = eStyleUp;
-	for (u8 u = 0; u < s->chord.note_count; u++) {
-		s->seq[u].note_idx = u;
-		s->seq[u].gate_length = 32; // TODO: figure out how this is determined/manipulated
+	for (u8 u = 0; u < c->note_count; u++) {
+		s->seq[u].note = c->notes[u];
+		s->seq[u].gate_length = 12; // TODO: figure out how this is determined/manipulated
 		s->seq[u].empty = 0;
-	}		
+	}
+	s->length = c->note_count;
 }
 
-static void arp_seq_build_down(arp_seq_t *s) {
-	s->style = eStyleUp;
-	u8 d = s->chord.note_count - 1;
-	for (u8 i = 0; i < s->chord.note_count; i++) {
-		s->seq[d].note_idx = i;
-		s->seq[d].gate_length = 32; // TODO: figure out how this is determined/manipulated
+static void arp_seq_build_down(arp_seq_t *s, chord_t *c) {
+	s->style = eStyleDown;
+	u8 d = c->note_count - 1;
+	for (u8 i = 0; i < c->note_count; i++) {
+		s->seq[d].note = c->notes[i];
+		s->seq[d].gate_length = 12; // TODO: figure out how this is determined/manipulated
 		s->seq[d].empty = 0;
 		d--;
-	}		
+	}
+	s->length = c->note_count;
 }
 
-static void arp_seq_build_converge(arp_seq_t *s) {
+static void arp_seq_build_converge(arp_seq_t *s, chord_t *c) {
 }
 
-static void arp_seq_build_diverge(arp_seq_t *s) {
+static void arp_seq_build_diverge(arp_seq_t *s, chord_t *c) {
 }
 
-static void arp_seq_build_random(arp_seq_t *s) {
+static void arp_seq_build_random(arp_seq_t *s, chord_t *c) {
 	random_state_t r;
 	s16 ri;
-	u8 count = s->chord.note_count;
+	u8 count = c->note_count;
 
 	s->style = eStyleRandom;
 
@@ -198,32 +188,89 @@ static void arp_seq_build_random(arp_seq_t *s) {
 			ri++;
 			if (ri == count) ri = 0;
 		}
-		s->seq[ri].note_idx = i;
-		s->seq[ri].gate_length = 32; // TODO: figure out how this is determined/manipulated
+		s->seq[ri].note = c->notes[i];
+		s->seq[ri].gate_length = 12; // TODO: figure out how this is determined/manipulated
 		s->seq[ri].empty = 0;
 	}
+	s->length = count;
 }
 
-static void arp_seq_build_played(arp_seq_t *s) {
+static void arp_seq_build_played(arp_seq_t *s, chord_t *c) {
+	// TODO: need some way to now order within chord
 }
 
+#define ARP_PPQ 24
 
 void arp_player_init(arp_player_t *p) {
 	p->ch = 0;
-	p->position = p->initial = 0;
+	p->start = 0;
 	p->octaves = 1;
 
-	p->velocity = eVelocityOriginal;
+	p->velocity = eVelocityPlayed;
 	p->gate = eGateFixed;
 
+	p->fixed_velocity = 127;
+	p->fixed_gate = 12;
+
 	p->latch = false;
+
+	arp_player_reset(p);
 }
 
-void arp_player_advance(arp_player_t *p, arp_seq_t *s, midi_behavior_t *b) {
-	// move forward one tick (where tick == 1/4? or 1/16)
+void arp_player_pulse(arp_player_t *p, arp_seq_t *s, midi_behavior_t *b) {
+	// move forward one tick (where tick == 1/4? or 1/16) => use 24ppq like midi
 	// ...call midi_note_on, midi_note_off as needed
+	u8 i, d, g, v;
+
+	p->tick_count++;
+	p->div_count++;
+	if (p->div_count == ARP_PPQ) {
+		// advance note reset
+		p->index++;
+		p->div_count = 0;
+	}
+	if (p->index == s->length) {
+		// sequence wrap
+		p->index = 0;
+		p->tick_count = 0;
+	}
+
+	i = p->index;
+	d = p->div_count;
+	
+	// which gate length
+	switch (p->gate) {
+	case eGateVariable:
+		g = s->seq[i].gate_length;
+	case eGateFixed:
+	default:
+		g = p->fixed_gate;
+		break;
+	}		
+
+	// note off if division matches gate length
+	if (d == g) {
+		b->note_off(p->ch, s->seq[i].note.num, 0);
+	}
+
+	// which velocity
+	switch (p->velocity) {
+	case eVelocityPlayed:
+		v = s->seq[i].note.vel;
+		break;
+	case eVelocityFixed:
+	default:
+		v = p->fixed_velocity;
+		break;
+	}
+
+	if (d == 0) {
+		b->note_on(p->ch, s->seq[i].note.num, v);
+	}
 }
 
 void arp_player_reset(arp_player_t *p) {
-	p->position = p->initial;
+	p->index = p->start;
+	p->div_count = 0;
+	p->tick_count = p->index * ARP_PPQ;
 }
--- src/arp.h
+++ src/arp.h
@@ -5,7 +5,7 @@
 #include "midi_common.h"
 
 #ifndef TEST
-#include "compiler.h"   // for bool
+#include "compiler.h"   // for bool; shouldn't this be "types.h"
 #else
 #include "test_bool.h"
 #endif
@@ -18,9 +18,9 @@
 #define CHORD_NOTE_MAX 127
 #define CHORD_VELOCITY_MAX 127
 
-//
-// constants
-//
+
+//-----------------------------
+//----- constants
 
 typedef enum {
 	eStyleUp,
@@ -35,7 +35,7 @@ typedef enum {
 
 typedef enum {
 	eVelocityFixed,
-	eVelocityOriginal
+	eVelocityPlayed
 } arp_velocity;
 
 typedef enum {
@@ -43,9 +43,9 @@ typedef enum {
 	eGateVariable
 } arp_gate;
 
-//
-// types
-//
+
+//-----------------------------
+//----- types
 
 typedef struct {
 	held_note_t notes[CHORD_MAX_NOTES];
@@ -53,31 +53,39 @@ typedef struct {
 } chord_t;
 
 typedef struct {
-	u8 note_idx;       // [0-CHORD_MAX_NOTES), which note in the code to play
-	u8 gate_length;    // [0-127], 0 is tie, 1-127 is fraction of beat (in ticks)?
+	//u8 note_idx;       // [0-CHORD_MAX_NOTES), which note in the code to play
+	held_note_t note;
+	u8 gate_length;    // [0-ARP_PPQ], 0 is tie, 1-ARP_PPQ is fraction of quater note
 	u8 empty : 1;
 } arp_note_t;
 
 typedef struct {
-	chord_t chord;
-
 	arp_style style;
 	arp_note_t seq[ARP_MAX_LENGTH];
+	u8 length;
 } arp_seq_t;
 	
 typedef struct {
-	u8 ch;           // channel; passed to midi behavior
-	u16 position;    // current position (in ticks)
-	u16 initial;     // initial position at start of cycle (in ticks)
+	u8 ch;              // channel; passed to midi behavior
+	u8 start;           // starting note index (on reset)
+	u8 index;           // currrent note index
+	u8 div_count;       // current sub-note position (in ticks)
+	u8 tick_count;      // current global position (in ticks)
 
-	u8 octaves;     // number of octaves
+	u8 octaves;         // number of octaves
 
 	arp_velocity velocity;
 	arp_gate gate;
 
-	bool latch;      //
+	u8 fixed_velocity;  // fixed velocity value [0-127]
+	u8 fixed_gate;      // fixed gate length [0-??]
+
+	bool latch;         //
 } arp_player_t;
 
+
+//-----------------------------
+//----- functions
 
 void chord_init(chord_t *c);
 bool chord_note_add(chord_t *c, u8 num, u8 vel);
@@ -86,13 +94,10 @@ s8   chord_note_low(chord_t *c);
 s8   chord_note_high(chord_t *c);
 
 void arp_seq_init(arp_seq_t *s);
-bool arp_seq_note_add(arp_seq_t *a, u8 num, u8 vel);
-bool arp_seq_note_release(arp_seq_t *a, u8 num);
-void arp_seq_build(arp_seq_t *a, arp_style style);
+void arp_seq_build(arp_seq_t *a, arp_style style, chord_t *c);
 
-// 
 void arp_player_init(arp_player_t* p);
-void arp_player_advance(arp_player_t *p, arp_seq_t *s, midi_behavior_t *b);
+void arp_player_pulse(arp_player_t *p, arp_seq_t *s, midi_behavior_t *b);
 void arp_player_reset(arp_player_t *a);
 
 #endif // __ARP__
