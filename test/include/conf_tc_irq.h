ab447b032e1e0ecdcee50468159fd3e163a1192e
Greg Wuller <greg@afofo.com>
2016-09-04 00:27:25 -0700

    test includes to redefine avr only functionality

---
+++ test/include/conf_tc_irq.h
@@ -0,0 +1,9 @@
+#ifndef __CONF_TC_IRQ_H__
+#define __CONF_TC_IRQ_H__
+
+// dummy; just get rid of them for now
+#define cpu_irq_disable_level(x)
+#define cpu_irq_enable_level(x)
+
+
+#endif // __CONF_TC_IRQ_H__
---
+++ test/include/print_funcs.h
@@ -0,0 +1,9 @@
+#ifndef __PRINT_FUNCS_H__
+#define __PRINT_FUNCS_H__
+
+#include <stdio.h>
+
+#define print_dbg(c) printf((c))
+#define print_dbg_ulong(n) printf("%d", (n))
+
+#endif
