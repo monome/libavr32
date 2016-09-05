863e557edd059cfa3bd05b61eb21792712ac75e7
Greg Wuller <greg@afofo.com>
2016-09-04 17:04:26 -0700

    added uclip: clip u32 value to range

--- src/util.h
+++ src/util.h
@@ -13,5 +13,6 @@ static volatile u32 x2 = 7654321;  // seed
 extern u32 rnd(void);
 extern u16 rotl(u16 value, u16 shift);
 char* itoa(int value, char* result, int base);
+extern u32 uclip(u32 value, u32 low, u32 high);
 
 #endif
