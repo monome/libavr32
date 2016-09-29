0df0e762ba7bb00907a69d4694c3d64e3b809f39
brian crabtree <tehn@monome.org>
2016-09-28 21:35:04 -0400

    faster tt debug speed

--- conf/teletype/conf_board.h
+++ conf/teletype/conf_board.h
@@ -96,7 +96,7 @@
 #define USART_IRQ                   AVR32_USART1_IRQ
 #define USART_IRQ_GROUP             AVR32_USART1_IRQ_GROUP
 #define USART_SYSCLK                SYSCLK_USART1
-#define USART_BAUDRATE 				57600
+#define USART_BAUDRATE 				115200
 
 
 /// compatibility with ASF print funcs
