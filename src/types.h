68c9b3ee1c82cfbebb7cf24ae8786e44d26e0f89
emb <emb@catfact.net>
2016-10-31 05:59:26 -0400

    guard against redefinition of basic data types

--- src/types.h
+++ src/types.h
@@ -1,7 +1,9 @@
 #pragma once
 
+
 #include <stdint.h>
 
+#ifndef DEFINED_BASIC_TYPES
 typedef int8_t       s8 ;  //!< 8-bit signed integer.
 typedef uint8_t      u8 ;  //!< 8-bit unsigned integer.
 typedef int16_t      s16;  //!< 16-bit signed integer.
@@ -12,3 +14,7 @@ typedef int64_t      s64;  //!< 64-bit signed integer.
 typedef uint64_t     u64;  //!< 64-bit unsigned integer.
 typedef float        f32;  //!< 32-bit floating-point number.
 typedef double       f64;  //!< 64-bit floating-point number.
+
+// use this to avoid redefinition errors
+#define DEFINED_BASIC_TYPES
+#endif
